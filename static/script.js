57ad6cdeee3fce854aeb359e9fcb0830239f0500
Vincenzo Copia <vincenzo.copia@gmail.com>
2024-04-21 19:29:35 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,348 @@
+var can = [
+    {
+        name: 'absolutely_zero',
+        value: 1,
+        type: "",
+        contry: "IT",
+        image: "absolutely_zero.webp",
+        title: "ABSOLUTELY ZERO",
+        text: ""
+    }, {
+        name: 'assalt',
+        value: 3,
+        type: "",
+        contry: "EN",
+        image: "assalt.webp",
+        title: "ASSALT",
+        text: ""
+    }, {
+        name: 'energy',
+        value: 1,
+        type: "",
+        contry: "IT",
+        image: "energy.webp",
+        title: "ENERGY",
+        text: ""
+    }, {
+        name: 'import',
+        value: 6,
+        type: "",
+        contry: "EN",
+        image: "import.webp",
+        title: "IMPORT",
+        ext: ""
+    }, {
+        name: 'irish_blend',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "irish_blend.webp",
+        title: "IRISH BLEND",
+        text: ""
+    }, {
+        name: 'kona_blend',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "kona_blend.webp",
+        title: "KONA BLEND",
+        text: ""
+    }, {
+        name: 'loca_moca',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "loca_moca.webp",
+        title: "LOCA MOCA",
+        text: ""
+    }, {
+        name: 'mango_loco',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "mango_loco.webp",
+        title: "MANGO LOCO",
+        text: ""
+    }, {
+        name: 'mean_bean',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "mean_bean.webp",
+        title: "MEAN BEAN",
+        text: ""
+    }, {
+        name: 'nitro',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "nitro.webp",
+        title: "NITRO",
+        text: ""
+    }, {
+        name: 'pacific_punch',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "pacific_punch.webp",
+        title: "PACIFIC PUNCH",
+        text: ""
+    }, {
+        name: 'papillon',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "papillon.webp",
+        title: "PAPILLON",
+        text: ""
+    }, {
+        name: 'pipeline_punch',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "pipeline_punch.webp",
+        title: "PIPELINE PUNCH",
+        text: ""
+    }, {
+        name: 'rehab',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "rehab.webp",
+        title: "REHAB",
+        text: ""
+    }, {
+        name: 'reserve_watermelon',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "reserve_watermelon.webp",
+        title: "RESERVE WATERMELON",
+        text: ""
+    }, {
+        name: 'ultra',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "ultra.webp",
+        title: "ULTRA",
+        text: ""
+    }, {
+        name: 'ultra_black',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "ultra_black.webp",
+        title: "ULTRA BLACK",
+        text: ""
+    }, {
+        name: 'ultra_blue',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "ultra_blue.webp",
+        title: "ULTRA BLUE",
+        text: ""
+    }, {
+        name: 'ultra_fiesta_mango',
+        value: 1,
+        type: "",
+        contry: "",
+        image: "ultra_fiesta_mango.webp",
+        title: "ULTRA FIESTA MANGO",
+        text: ""
+    }, {
+        name: 'ultra_gold',
+        value: 1,
+        image: "ultra_gold.webp",
+        title: "ULTRA GOLD",
+        text: ""
+    }, {
+        name: 'ultra_paradise',
+        value: 1,
+        image: "ultra_paradise.webp",
+        title: "ULTRA PARADISE",
+        text: ""
+    }, {
+        name: 'ultra_peachy_keen',
+        value: 1,
+        image: "ultra_peachy_keen.webp",
+        title: "ULTRA PEACHY KEEN",
+        text: ""
+    }, {
+        name: 'ultra_red',
+        value: 1,
+        image: "ultra_red.webp",
+        title: "ULTRA RED",
+        text: ""
+    }, {
+        name: 'ultra_rosa',
+        value: 1,
+        image: "ultra_rosa.webp",
+        title: "ULTRA ROSA",
+        text: ""
+    }, {
+        name: 'ultra_bsunrise',
+        value: 1,
+        image: "ultra_sunribe.webp",
+        title: "ULTRA SUNRISE",
+        text: ""
+    }, {
+        name: 'ultra_violet',
+        value: 1,
+        image: "ultra_violet.webp",
+        title: "ULTRA VIOLET",
+        text: ""
+    }, {
+        name: 'ultra_watermelon',
+        value: 1,
+        image: "ultra_watermelon.webp",
+        title: "ULTRA WATERMELON",
+        text: ""
+    }, {
+        name: 'vanilla_light',
+        value: 1,
+        image: "vanilla_light.webp",
+        title: "VANILLA LIGHT",
+        text: ""
+    }, {
+        name: 'vr46',
+        value: 1,
+        image: "vr46.webp",
+        title: "VR46",
+        text: ""
+    }, {
+        name: 'white_pineapple',
+        value: 1,
+        image: "white_pineapple.webp",
+        title: "WHITE PINEAPPLE",
+        text: ""
+    }, {
+        name: 'cosmic_peach',
+        value: 3.5,
+        image: "cosmic_peach.webp",
+        title: "COSMIC PEACH",
+        text: ""
+    }, {
+        name: 'zero_sugar',
+        value: 1,
+        image: "zero_sugar.webp",
+        title: "ZERO SUGAR",
+        text: ""
+    }, {
+        name: 'espresso_vanilla',
+        value: 2,
+        image: "espresso_vanilla.webp",
+        title: "ESPRESSO VANILLA",
+        text: ""
+    }, {
+        name: 'espresso',
+        value: 2,
+        image: "espresso.webp",
+        title: "ESPRESSO",
+        text: ""
+    }, {
+        name: 'ultra_fiesta',
+        value: 2,
+        image: "ultra_fiesta.webp",
+        title: "ULTRA FIESTA",
+        text: ""
+    }, {
+        name: 'aussie_lemonade',
+        value: 2,
+        image: "aussie_lemonade.webp",
+        title: "ASSIE LEMONADE",
+        text: ""
+    }, {
+        name: 'mixxd',
+        value: 2,
+        image: "mixxd.webp",
+        title: "MIXXD",
+        text: ""
+    }, {
+        name: 'khaotic',
+        value: 2,
+        image: "khaotic.webp",
+        title: "KHAOTIC",
+        text: ""
+    }, {
+        name: 'monarch',
+        value: 2,
+        image: "monarch.webp",
+        title: "MONARCH",
+        text: ""
+    }, {
+        name: 'ripper',
+        value: 4,
+        image: "ripper.webp",
+        title: "RIPPER",
+        text: ""
+    }, {
+        name: 'iced_tea_lemon',
+        value: 2,
+        image: "iced_tea_lemon.webp",
+        title: "ICED TEA LEMON",
+        text: ""
+    }, {
+        name: 'iced_tea_peach',
+        value: 2,
+        image: "iced_tea_peach.webp",
+        title: "ICED TEA PEACH",
+        text: ""
+    }, {
+        name: 'recover',
+        value: 2,
+        image: "recover.webp",
+        title: "RECOVER",
+        text: ""
+    }, {
+        name: 'lewis_hamilton_zero',
+        value: 1,
+        image: "lewis_hamilton_zero.webp",
+        title: "LEWIS HAMILTON ZERO",
+        text: ""
+    }, {
+        name: 'lewis_hamilton',
+        value: 1,
+        image: "lewis_hamilton.webp",
+        title: "LEWIS HAMILTON",
+        text: ""
+    }, {
+        name: 'salted_caramel',
+        value: 1,
+        image: "salted_caramel.webp",
+        title: "SALTED CARAMEL",
+        text: ""
+    }, {
+        name: 'ultra_golden',
+        value: 1,
+        image: "ultra_golden_pineapple.webp",
+        title: "ULTRA GOLDEN PINEAPPLE",
+        text: ""
+    }, {
+        name: 'ultra_citron',
+        value: 1,
+        image: "ultra_citron.webp",
+        title: "ULTRA CITRON",
+        text: ""
+    }, {
+        name: 'mule',
+        value: 1,
+        image: "mule.webp",
+        title: "MULE",
+        text: ""
+    }, {
+        name: 'extra_strength',
+        value: 1,
+        image: "extra_strength.webp",
+        title: "EXTRA STRENGTH 3",
+        text: ""
+    }, {
+        name: 'm',
+        value: 1,
+        image: "waiting.webp",
+        title: " ",
+        text: ""
+    }
+
+];
